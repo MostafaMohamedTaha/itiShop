2c3d3d825cee539b2e0c7dd8f6f4108d1e6571b1
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-12-19 11:29:54 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,46 @@
+//#region default 
+
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//#endregion
+//#region shared
+
+import { HeaderComponent } from './components/shared/header/header.component';
+
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { HomeComponent } from './components/component/home/home.component';
+import { ProductComponent } from './components/component/product/product.component';
+//#endregion
 
 @NgModule({
   declarations: [
-    AppComponent
+    //#region default 
+
+    AppComponent,
+    HeaderComponent,
+    
+    FooterComponent,
+         SidebarComponent,
+         HomeComponent,
+         ProductComponent
+    //#endregion
   ],
   imports: [
+    //#region default
+
     BrowserModule,
     AppRoutingModule
+    //#endregion
   ],
   providers: [
+    //#region default
+
     provideClientHydration()
+    //#endregion
   ],
   bootstrap: [AppComponent]
 })
